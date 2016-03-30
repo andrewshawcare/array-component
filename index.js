81a4453e702bfbc5a1a7e320f954b331d28ff80f
Andrew Shaw Care <andrewshawcare@gmail.com>
2016-03-30 08:55:22 -0500

    Add valueComponent

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-ArrayComponent = function (array) {
+ArrayComponent = function (array, valueComponent) {
+  var valueComponent = valueComponent || function (value) {
+    var valueElement = document.createElement("span");
+    valueElement.innerText = value;
+    return valueElement;
+  };
   var arrayElement = document.createElement("ol");
   arrayElement.classList.add("array");
-  array.forEach(function (item) {
+  array.forEach(function (value) {
     var itemElement = document.createElement("li");
     itemElement.classList.add("item");
-    itemElement.innerText = item;
+    itemElement.appendChild(valueComponent(value));
     arrayElement.appendChild(itemElement);
   });
   return arrayElement;
