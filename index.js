e887fd7e44fe3cda4f2a63d1e758122a1324d085
Andrew Shaw Care <andrewshawcare@gmail.com>
2016-05-22 15:10:41 -0500

    Add ValueComponent

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ define([], function () { "use strict";
   return function (data) {
     var data = data || {};
     var array = data.array || [];
-    var valueComponent = data.valueComponent || function (value) {
-          var valueElement = document.createElement("span");
-          valueElement.innerText = value;
-          return valueElement;
-        };
+    var ValueComponent = data.ValueComponent || function (data) {
+      var value = data.value || "";
+      var valueElement = document.createElement("span");
+      valueElement.textContent = value;
+      return valueElement;
+    };
 
     var arrayElement = document.createElement("ol");
     arrayElement.classList.add("array");
     array.forEach(function (value) {
       var itemElement = document.createElement("li");
-      itemElement.classList.add("item");
-      itemElement.appendChild(valueComponent(value));
+      itemElement.classList.add("value");
+      itemElement.appendChild(ValueComponent({value: value}));
       arrayElement.appendChild(itemElement);
     });
 
--- spec.js
+++ spec.js
@@ -3,7 +3,7 @@ define(["./index.js"], function (ArrayComponent) {
   describe("An array component", function () {
     it("creates an ordered list from an array", function () {
       var array = ["apple", "carrot", "steak"];
-      var arrayComponent = ArrayComponent(array);
+      var arrayComponent = ArrayComponent({array: array});
       expect(arrayComponent.classList).toContain("array");
     });
   });
